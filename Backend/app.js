db814a8f6bef02ce779dba9481c373524c675a6f
Moustaquim Younes <moustaquimy@gmail.com>
2024-01-07 00:20:25 +0100

    Update app.js
    Adding  CRUD operations related to Quizzes (Create, Read, Update, and Delete)

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,62 @@
 const express = require('express');
 const app = express();
+const { mongoose } = require('./db/mongoose');
+const bodyParser = require('body-parser');
 const port = 3000;
+
+
+const User = require('./db/models/users.model');
+const Quiz = require('./db/models/quiz.model');
+const Question = require('./db/models/questions.model');
+
+
+
+//load middlweare
+app.use(bodyParser.json());
+
+
+//Quiz HTTP Services
+
+    app.get('/quiz', (req, res) => {
+      // return an array of all quizzes
+      Quiz.find({}).then((quizzes) => {
+          res.send(quizzes);
+      });
+    });
+
+    app.post('/quiz', (req, res) => {
+      // create new quiz
+      let quizname = req.body.quizname;
+      let quizdescription = req.body.quizdescription;
+      let newQuiz = new Quiz({ quizname, quizdescription: req.body.quizdescription });
+      newQuiz.save().then((quizDoc) => {
+          res.send(quizDoc);
+      });
+    });
+
+    app.patch('/quiz/:id', (req, res) => {
+      // update a quiz
+      Quiz.findOneAndUpdate(
+          { _id: req.params.id },
+          { $set: req.body }
+      ).then(() => {
+          res.sendStatus(200);
+      });
+    });
+
+
+    app.delete('/quiz/:id', (req, res) => {
+      // delete a quiz
+      Quiz.findOneAndDelete({
+          _id: req.params.id
+      }).then((removedQuizDoc) => {
+          res.send(removedQuizDoc);
+      });
+    });
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello, Express!');

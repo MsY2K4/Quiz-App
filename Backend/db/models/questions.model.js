6fe1f57a5ef72784ad11371d0b67dfbd300930ec
Moustaquim Younes <moustaquimy@gmail.com>
2024-01-07 00:18:04 +0100

    Create questions.model.js
    Creating Question Schema

---
+++ Backend/db/models/questions.model.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose')
+var questionSchema = mongoose.Schema({
+    quizid: {
+        type: String,
+        required: true
+    },
+    questionId: {
+        type: String,
+        required: true
+    },
+    questionText:{
+        type: String, 
+        required: true
+    },
+    answer: {
+        type: String,
+        required: true
+    },
+    options:{
+        type  :Array,
+        default:[]
+    }
+})
+
+const Question= mongoose.model('Question',questionSchema);
+module.exports = Question;

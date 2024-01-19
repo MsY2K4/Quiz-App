d2f290ebc9ac33a8f88526b07bd7c68b154a8fab
Moustaquim Younes <moustaquimy@gmail.com>
2024-01-19 22:17:56 +0100

    Update auth.service.ts

--- my_quiz_app/src/app/auth/auth.service.ts
+++ my_quiz_app/src/app/auth/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:3000';  // Ensure this matches your server's URL
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+  login(email: string, password: string): Observable<any> {
+    const loginData = { email, password };
+    return this.http.post<any>(`${this.apiUrl}/login`, loginData);
+  }
+
+  register(email: string, password: string): Observable<any> {
+    const registerData = { email, password };
+    return this.http.post<any>(`${this.apiUrl}/register`, registerData);
+  }
 }

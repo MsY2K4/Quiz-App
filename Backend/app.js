a698d6323f6083b6eb37e65c42e5a7ffa9a7da53
Moustaquim Younes <moustaquimy@gmail.com>
2024-01-19 22:28:52 +0100

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const { mongoose } = require('./db/mongoose');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const secretKey = 'younes@younes@12';
 const port = 3000;
 
 
@@ -10,15 +13,101 @@ const Quiz = require('./db/models/quiz.model');
 const Question = require('./db/models/questions.model');
 
 
-
-//load middlweare
+app.use(cors());
 app.use(bodyParser.json());
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+function authenticateToken(req, res, next) {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) return res.status(403).json({ message: 'Invalid token.' });
+    req.user = user;
+    next();
+  });
+}
+
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email, password });
+    if (!user) {
+      return res.status(401).json({ success: false, message: 'Invalid credentials' });
+    }
+
+    console.log('User logged in successfully:', user);
+
+    const token = jwt.sign({ userId: user._id, email: user.email }, secretKey, { expiresIn: '1h' });
+    res.json({ success: true, token });
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).json({ success: false, message: 'Internal Server Error', error: error.message });
+  }
+});
+
+
+app.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const newUser = new User({ email, password });
+    const user = await newUser.save();
+
+    console.log('User registered successfully:', user);
+
+    const token = jwt.sign({ userId: user._id, email: user.email }, secretKey, { expiresIn: '1h' });
+    res.json({ success: true, token });
+  } catch (error) {
+    console.error('Error during user registration:', error);
+    res.status(500).json({ success: false, message: 'Internal Server Error', error: error.message });
+  }
+});
+
+
+//Quiz HTTP Services
+
+    app.get('/quiz', (req, res) => {
+      // return an array of all quizzes
+      Quiz.find({}).then((quizzes) => {
+          res.send(quizzes);
+      });
+    });
+
+    app.post('/quiz', (req, res) => {
+      // create new quiz
+      const quizname = req.body.quizname;
+      const quizdescription = req.body.quizdescription;
+      const newQuiz = new Quiz({ quizname, quizdescription });
+      newQuiz.save().then((quizDoc) => {
+          res.send(quizDoc);
+      });
+    });
+
+    app.patch('/quiz/:id', (req, res) => {
+      // update a quiz
+      Quiz.findOneAndUpdate(
+          { _id: req.params.id },
+          { $set: req.body }
+      ).then(() => {
+          res.sendStatus(200);
+      });
+    });
+
+
+    app.delete('/quiz/:id', (req, res) => {
+      // delete a quiz
+      Quiz.findOneAndDelete({
+          _id: req.params.id
+      }).then((removedQuizDoc) => {
+          res.send(removedQuizDoc);
+      });
+    });
 
 
 //Quiz HTTP Services

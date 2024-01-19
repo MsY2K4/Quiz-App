5fc7b5e9b0f2ae6dc66e06ae4d7c3044b2c7b322
Moustaquim Younes <moustaquimy@gmail.com>
2024-01-19 22:10:36 +0100

    Update users.model.js

--- Backend/db/models/users.model.js
+++ Backend/db/models/users.model.js
@@ -1,7 +1,25 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const userSchema = mongoose.Schema({
-
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    match: /^\S+@\S+\.\S+$/,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6, // Minimum password length
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user',
+  },
 });
 
 const User = mongoose.model('User', userSchema);

827286030abd82d14497a1e7a6b4d4d55532151b
Moustaquim Younes <moustaquimy@gmail.com>
2024-01-19 22:19:08 +0100

    Update register.component.ts

--- my_quiz_app/src/app/auth/register/register.component.ts
+++ my_quiz_app/src/app/auth/register/register.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  register(): void {
+    const email = (document.getElementById('email') as HTMLInputElement).value;
+    const password = (document.getElementById('password') as HTMLInputElement).value;
+
+    this.authService.register(email, password).subscribe(
+      (response) => {
+        if (response && response.token) {
+          localStorage.setItem('token', response.token);
+          console.log('Registered successfully');
+          this.router.navigate(['/login']);
+        } else {
+          console.error('Invalid token received');
+        }
+      },
+      (error) => {
+        console.error('Registration failed:', error);
+      }
+    );
+  }
 }

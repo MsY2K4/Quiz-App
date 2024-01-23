bd55ffc3c4ba3e6d08c6171d694c8f7f740bc51b
Moustaquim Younes <moustaquimy@gmail.com>
2024-01-23 15:40:04 +0100

    Update quiz.service.ts

--- my_quiz_app/src/app/quiz.service.ts
+++ my_quiz_app/src/app/quiz.service.ts
@@ -7,7 +7,7 @@ import { WebRequestService } from './web-request.service';
 export class QuizService {
 
   constructor(private webReqService: WebRequestService) { }
-
+  //QUIZ
   createQuiz(name: string, description: string){
    return this.webReqService.post('quiz', { quizname: name, quizdescription: description });
   }
@@ -15,12 +15,17 @@ export class QuizService {
   fetchQuizes() {
     return this.webReqService.get("quiz");
   }
-  
+  deleteQuiz(quizId: string) {
+    return this.webReqService.delete(`quiz/${quizId}`);
+  }
+  //QUESTION
   fetchQuestions(quizId: string) {
     return this.webReqService.get(`quiz/${quizId}/questions`);
   }
   addQuestion(quizId: string, question: any) {
     return this.webReqService.post(`quiz/${quizId}/questions`, question);
   }
-
+  deleteQuestion(quizId: string, questionId: string) {
+    return this.webReqService.delete(`quiz/${quizId}/questions/${questionId}`);
+  }
 }

e88c796579e0a402b4da8a3f2d277728bd2c19aa
Moustaquim Younes <moustaquimy@gmail.com>
2024-01-26 18:32:31 +0100

    Update app.component.ts
    Dynamic user authentication status and role updates.

--- my_quiz_app/src/app/app.component.ts
+++ my_quiz_app/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Mindscape';
+export class AppComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  isAdmin: boolean = false;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    this.updateUserRole();
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updateUserRole();
+      }
+    });
+  }
+
+  updateUserRole() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.isLoggedIn = true;
+      this.authService.getUserRole(token).subscribe(
+        (response) => {
+          if (response.role === 'admin') {
+            this.isAdmin = true;
+          }
+        },
+        (error) => {
+          console.error('Error fetching user role:', error);
+        }
+      );
+    }
+  }
+  
+  onLogout() {
+    this.isLoggedIn = false;
+    this.isAdmin = false;
+  }
 }
